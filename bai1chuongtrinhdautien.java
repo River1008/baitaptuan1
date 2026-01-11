6a94b4ecae966bd323445b4b84e727049a793b87
Lê Trường Giang <0850080068@sv.hcmunre.edu.vn>
2026-01-11 22:50:54 +0700

    Delete bai1chuongtrinhdautien.java

--- bai1chuongtrinhdautien.java
+++
@@ -1,8 +0,0 @@
-package tuan1lthdt;
-
-public class bai1chuongtrinhdautien {
-    public static void main(String[] args) {
-          System.out.println("xin chao cac ban sinh vien dtm");
-          System.out.println("day la chuong trinh dau tien");
-    }
-}
