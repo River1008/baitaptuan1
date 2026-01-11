abf351bc05c49f40e0b012ff42390fc9974ed4e7
Lê Trường Giang <0850080068@sv.hcmunre.edu.vn>
2026-01-11 22:51:02 +0700

    Delete bai2gioithieu.java

--- bai2gioithieu.java
+++
@@ -1,9 +0,0 @@
-package tuan1lthdt;
-
-public class bai2gioithieu {
-    public static void main(String[] args) {
-          System.out.println("Le Truong Giang");
-          System.out.println("0850080068");
-          System.out.println("08_DH_THMT");
-    }
-}
